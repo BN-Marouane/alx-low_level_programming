541590cdd09c3e858d907a2596fefe0a5fcb4c89
Bn-Marouane <bnmarouane30@gmail>
2023-03-21 15:35:55 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,21 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Description: print _putchar
- *
+ * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char putch[] = "_putchar";
-
-	int i;
-
-	for (i = 0; i <= 7; i++)
-	{
-		_putchar(putch[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
