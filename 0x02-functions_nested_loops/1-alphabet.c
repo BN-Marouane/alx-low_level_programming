050d91031200a44892e0db1ed8ac93251d844f37
Bn-Marouane <bnmarouane30@gmail>
2023-03-21 15:32:46 -0700

    Done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char al = 'a';
+
+	while (al <= 'z')
+	{
+		_putchar(al);
+		al++;
+	}
+	_putchar('\n');
+}
