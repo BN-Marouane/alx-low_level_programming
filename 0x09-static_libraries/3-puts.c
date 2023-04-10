9ad55bca08ddf3a6f797e87fc1494d9a3de78265
Bn-Marouane <bnmarouane30@gmail>
2023-04-10 15:42:24 -0700

    Done

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string, followed by a new line.
+ *
+ * @str: string input parameter.
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  * _puts - prints a string, followed by a new line.
+ *   *
+ *    * @str: string input parameter.
+ *     */
+
+void _puts(char *str)
+{
+	puts(str);
+}
